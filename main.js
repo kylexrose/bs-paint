640545885eb1f8477c05bb00724d9b901cace424
kylexrose@gmail.com <kylexrose@gmail.com>
2021-03-30 15:09:53 -0400

    fixed undefined bug on palette

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const gridWidth = 20;
 let count = 0;
-while (count <= gridWidth * gridWidth) {
+while (count < gridWidth * gridWidth) {
   const canvas = document.querySelector('.canvas');
   const div = document.createElement('div');
   div.className = 'square color-8';
@@ -28,12 +28,13 @@ const colors = [];
 for (let i = 1; i <= numOfColors; i++){
   colors.push(`color-${i}`);
 }
+
 const palette = document.querySelector(".palette");
 const canvas = document.querySelector(".canvas");
 let brushColor = document.querySelector('#current-brush').classList;
 
 //palette handler
-palette.addEventListener('click', (e) => {
+palette.addEventListener('mouseup', (e) => {
   changeColor(brushColor, e.target.classList[1])
 })
 
@@ -47,6 +48,5 @@ function changeColor(elementBeingColored, elementColoring){
     for(let color of colors){
       elementBeingColored.remove(color);
     }
-    elementBeingColored.remove("undefined");
     elementBeingColored.add(elementColoring);
 }
